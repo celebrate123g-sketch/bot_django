53acc163949d9ba2eb493998467b89446615c8ed
Springtrap <springtrap@gmail.com>
2026-03-30 21:55:12 +0500

    Initial commit: Django project

---
+++ main/views.py
@@ -0,0 +1,111 @@
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView
+from django.template.response import TemplateResponse
+from django.db.models import Q
+from .models import Category, Product, Size
+from django.views.generic import DetailView
+
+class IndexView(TemplateView):
+    template_name = 'main/base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['current_category'] = None
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        if request.headers.get('HX-Request'):
+            return TemplateResponse(request, 'main/base.html', context)
+
+        return TemplateResponse(request, self.template_name, context)
+
+
+class CatalogView(TemplateView):
+    template_name = 'main/base.html'
+
+    FILTER_MAPPING = {
+        'color': lambda queryset, value: queryset.filter(color__iexact=value),
+        'min_price': lambda queryset, value: queryset.filter(price__gte=value),
+        'max_price': lambda queryset, value: queryset.filter(price__lte=value),
+        'size': lambda queryset, value: queryset.filter(product_size__size__name=value),
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        category_slug = kwargs.get('category_slug')
+        categories = Category.objects.all()
+        products = Product.objects.all().order_by('-created_at')
+        current_category = None
+
+        if category_slug:
+            current_category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=current_category)
+
+        query = self.request.GET.get('q')
+        if query:
+            products = products.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+
+        filter_params = {}
+
+        for param, filter_func in self.FILTER_MAPPING.items():
+            value = self.request.GET.get(param)
+
+            if value:
+                products = filter_func(products, value)
+                filter_params[param] = value
+            else:
+                filter_params[param] = ''
+
+        filter_params['q'] = query or ''
+
+        context.update({
+            'categories': categories,
+            'products': products,
+            'current_category': current_category,
+            'filter_params': filter_params,
+            'sizes': Size.objects.all(),
+            'search_query': query or ''
+        })
+
+        if self.request.GET.get('show_search') == 'true':
+            context['show_search'] = True
+
+        elif self.request.GET.get('reset_search') == 'true':
+            context['reset_search'] = True
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        if request.headers.get('HX-Request'):
+
+            if context.get('show_search'):
+                return TemplateResponse(request, 'main/search_input.html', context)
+
+            elif context.get('reset_search'):
+                return TemplateResponse(request, 'main/search_button.html', {})
+
+            template = (
+                'main/filter_modal.html'
+                if request.GET.get('show_filters') == 'true'
+                else 'main/catalog.html'
+            )
+
+            return TemplateResponse(request, template, context)
+
+        return TemplateResponse(request, self.template_name, context)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'main/product_detail.html'
+    context_object_name = 'product'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
